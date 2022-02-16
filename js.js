9ecf00b6e7a44956c8aa6ce9cf2a68845b8b8f2c
BakasuraRCE <bakasura@protonmail.ch>
2022-02-16 08:57:04 -0500

    feat: implement jquery in all html feat: update jquery to 3.6.0 feat(download): replace obsolete js style by css

--- js.js
+++ js.js
@@ -1,7 +1,15 @@
 function ver_MouseOverTR(tr) {
-	tr.style.backgroundColor='#464A6B';
+    tr.style.backgroundColor = '#464A6B';
 }
 
 function ver_MouseOutTR(tr) {
-	tr.style.backgroundColor='';
+    tr.style.backgroundColor = '';
 }
+
+$(function () {
+    $(".file-links a, .sub-file-links a").hover(
+        function () {
+            $(this).closest('.download-box').toggleClass('active')
+        }
+    )
+})
