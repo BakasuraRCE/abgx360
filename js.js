593a00cefdc0e5aab5e4a24bfd1f39ab34ec6011
BakasuraRCE <bakasura@protonmail.ch>
2022-02-15 22:35:32 -0500

    fix: remove GA3 link evnet

--- js.js
+++ js.js
@@ -1,10 +1,3 @@
-function RecLink(link, category, action) {
-	try {
-		pageTracker._trackEvent(category, action);
-		setTimeout('document.location = "' + link.href + '"', 100)
-	} catch(err) {}
-}
-
 function ver_MouseOverTR(tr) {
 	tr.style.backgroundColor='#464A6B';
 }
