0f2a45abfeaf134b7fb009bf697d1eed211edb75
BakasuraRCE <bakasura@protonmail.ch>
2022-02-21 18:01:20 -0500

    fix: update globals for publishconf

--- publishconf.py
+++ publishconf.py
@@ -1,7 +1,9 @@
 import sys
+
 sys.path.append('.')
 from pelicanconf import *
 
 SITEURL = 'https://bakasurarce.github.io/abgx360'
+JINJA_GLOBALS['SITEURL'] = SITEURL
 
 PLUGINS += ['html-minifier']
