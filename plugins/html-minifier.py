9184f05fc2cf249ff25b1c24c00f845e2a68f6c9
BakasuraRCE <bakasura@protonmail.ch>
2022-02-22 22:50:27 -0500

    seo: add css and js minifier

--- plugins/html-minifier.py
+++ plugins/html-minifier.py
@@ -3,21 +3,25 @@ html-minifier wrapper for Pelican
 """
 
 import glob
-import os
-import sys
 import subprocess
 
 from pelican import signals
 
-cmd = ['./node_modules/.bin/html-minifier', '--config-file', 'html-minifier.conf']
+cmd_html_minifier = ['./node_modules/.bin/html-minifier', '--config-file', 'html-minifier.conf']
+cmd_terser = ['./node_modules/.bin/terser', '--comments', 'false']
+cmd_cleancss = ['./node_modules/.bin/cleancss', '-O1', 'specialComments:0']
 
 
 def process_file(file_path: str) -> None:
     try:
-        subprocess.run(cmd + [file_path, '--output', file_path], stderr=subprocess.DEVNULL, check=True)
-        print('OK:', file_path, os.getcwd())
+        if file_path.endswith('.js'):
+            subprocess.run(cmd_terser + [file_path, '-o', file_path], stderr=subprocess.DEVNULL, check=True)
+        elif file_path.endswith('.css'):
+            subprocess.run(cmd_cleancss + [file_path, '-o', file_path], stderr=subprocess.DEVNULL, check=True)
+        else:
+            subprocess.run(cmd_html_minifier + [file_path, '--output', file_path], stderr=subprocess.DEVNULL, check=True)
     except subprocess.CalledProcessError:
-        print('ERROR:', file_path, os.getcwd())
+        print('ERROR:', file_path)
 
 
 def main(pelican):
@@ -26,10 +30,10 @@ def main(pelican):
         process_file(f)
     for f in glob.iglob(pelican.output_path + '/**/*.css', recursive=True):
         process_file(f)
-    # for f in glob.iglob(pelican.output_path + '/**/*.js', recursive=True):
-    #     if f.endswith('min.js'):
-    #         continue
-    #     process_file(f)
+    for f in glob.iglob(pelican.output_path + '/**/*.js', recursive=True):
+        if f.endswith('min.js'):
+            continue
+        process_file(f)
 
 
 def register():
